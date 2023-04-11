aba14e47508d6d7169c9e5fd002643bf579ceb40
Gary Wee <51909547+weegary@users.noreply.github.com>
2023-04-11 22:58:18 +0800

    Create KD.cs

---
+++ Data/Indicator/KD.cs
@@ -0,0 +1,69 @@
+namespace Stock.Data.Indicator
+{
+    public class KD
+    {
+        /// <summary>
+        /// This function calculates the RSV, K and D values. 
+        /// For the candlesticks before rsvPeriod, the values will be -1.
+        /// </summary>
+        /// <param name="candlesticks"></param>
+        /// <param name="rsvPeriod"></param>
+        /// <param name="kPeriod"></param>
+        /// <param name="dPeriod"></param>
+        /// <returns></returns>
+        public static (decimal[] RSV, decimal[] K, decimal[] D) RSV_KD(Candlestick[] candlesticks, int rsvPeriod = 9, int kPeriod = 3, int dPeriod = 3, decimal pre_k = -1, decimal pre_d = -1)
+        {
+            decimal[] RSV = new decimal[candlesticks.Length];
+            decimal[] K = new decimal[candlesticks.Length];
+            decimal[] D = new decimal[candlesticks.Length];
+
+            for (int i = 0; i < candlesticks.Length; i++)
+            {
+                if (i < rsvPeriod - 1)
+                {
+                    RSV[i] = -1;
+                    K[i] = -1;
+                    D[i] = -1;
+                    continue;
+                }
+                decimal min = candlesticks[i].LowPrice;
+                decimal max = candlesticks[i].HighPrice;
+                for (int j = i - (rsvPeriod - 1); j <= i; j++)
+                {
+                    if (min > candlesticks[j].LowPrice)
+                        min = candlesticks[j].LowPrice;
+                    if (max < candlesticks[j].HighPrice)
+                        max = candlesticks[j].HighPrice;
+                }
+                decimal temp1 = (max - min);
+                decimal temp2 = (candlesticks[i].ClosePrice - min);
+                int index = i; 
+                if ((max - min) == 0)
+                    RSV[index] = 100;
+                else
+                    RSV[index] = (decimal)Math.Round((Math.Round((candlesticks[i].ClosePrice - min), 2) / Math.Round((max - min), 2) * 100), 3);
+                
+                decimal pre_K, pre_D;
+                if (index == rsvPeriod - 1)
+                {
+                    pre_K = 50;
+                    pre_D = 50;
+                    if (pre_k != -1)
+                        pre_K = pre_k;
+                    if (pre_d != -1)
+                        pre_D = pre_d;
+                }
+                else
+                {
+                    pre_K = K[index - 1];
+                    pre_D = D[index - 1];
+                }
+                K[index] = pre_K * 2 / kPeriod + (RSV[index] / kPeriod);
+                D[index] = pre_D * 2 / dPeriod + (K[index] / dPeriod);
+                K[index] = Math.Round(K[index],2);
+                D[index] = Math.Round(D[index],2);
+            }
+            return (RSV, K, D);
+        }
+    }
+}
